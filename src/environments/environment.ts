e455479c6cbde7fe25031281f7b4f50a4823711c
Nam <namandroid.it@gmail.com>
2023-01-10 19:30:34 +0700

    hihi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,12 +2,12 @@
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
-const ip = '1.117.181.242';
-const port = '80';
+const ip = '117.2.198.251';
+const port = '3002';
 export const localUrl = `http://${ip}:${port}`;
 
 export const environment = {
-  production: false
+  production: true
 };
 
 /*
