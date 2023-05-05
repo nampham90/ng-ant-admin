9c9e88e174c859fbbff3e6e5be0ec75d18eaeee0
Nam <namandroid.it@gmail.com>
2023-05-05 19:38:33 +0700

    hihi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // `ng build` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
-//const ip = '117.2.212.47';
-const ip = 'localhost';
+const ip = '117.2.212.47';
+//const ip = 'localhost';
 const port = '3001';
 
 
